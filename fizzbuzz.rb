bd458f6d2f5812bf5e93b93ec351db81e3c61f9b
Kayenga Campos <31307359+Kissabi@users.noreply.github.com>
2019-12-22 05:21:40 -0300

    Add files via upload

---
+++ fizzbuzz.rb
@@ -0,0 +1,19 @@
+#Um exercício divertido no ruby-WinOS
+(1..100).each do |num|
+if num % 2 == 0
+system "color 2"
+  puts "#{num}. Divisivel por 2. Resultado: #{num/2}"
+elsif num % 3 == 0 && num % 5 == 0
+system "color 2"
+  puts "#{num}. Divisivel por 3 e por 5. Resultados: #{num/3}, #{num/5}"
+elsif num % 3 == 0
+system "color 2"
+  puts "#{num}. Divisivel por 3. Resultado: #{num/3}"
+else
+system "color 3"
+  puts "#{num}. Indivisivel. Resultados: #{num/2.0}, #{num/3.0}, #{num/5.0} "
+ end
+  end
+ system "pause"
+
+
